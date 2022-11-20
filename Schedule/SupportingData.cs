9daa64aae7d711436963aa9e123d7e4646bf0f23
Dennis Dunn <dennisdunn@users.noreply.github.com>
2022-11-20 09:10:26 -0500

    The partial class idea is a bust.

--- Schedule/SupportingData.cs
+++ Schedule/SupportingData.cs
@@ -1,20 +1,17 @@
 ﻿using System.Collections.Generic;
 using OpenCdsi.Schedule;
 
-namespace System
+namespace OpenCdsi
 {
-    public partial class Cdsi
+    public static class SupportingData
     {
-        public static class Schedule
-        {
-            internal static scheduleSupportingData _schedule = Factories.ReadScheduleData();
+        internal static scheduleSupportingData _schedule = Factories.ReadScheduleData();
 
-            internal static IDictionary<string, antigenSupportingData> _antigens = Factories.ReadAntigenData();
-            public static IEnumerable<scheduleSupportingDataLiveVirusConflict> LiveVirusConflicts => _schedule.liveVirusConflicts;
-            public static IEnumerable<scheduleSupportingDataVaccineGroup> VaccineGroups => _schedule.vaccineGroups;
-            public static IEnumerable<scheduleSupportingDataCvxMap> Vaccines => _schedule.cvxToAntigenMap;
-            public static IEnumerable<scheduleSupportingDataObservation> Observations => _schedule.observations;
-            public static IDictionary<string, antigenSupportingData> Antigens => _antigens;
-        }
+        internal static IDictionary<string, antigenSupportingData> _antigens = Factories.ReadAntigenData();
+        public static IEnumerable<scheduleSupportingDataLiveVirusConflict> LiveVirusConflicts => _schedule.liveVirusConflicts;
+        public static IEnumerable<scheduleSupportingDataVaccineGroup> VaccineGroups => _schedule.vaccineGroups;
+        public static IEnumerable<scheduleSupportingDataCvxMap> Vaccines => _schedule.cvxToAntigenMap;
+        public static IEnumerable<scheduleSupportingDataObservation> Observations => _schedule.observations;
+        public static IDictionary<string, antigenSupportingData> Antigens => _antigens;
     }
 }
--- UnitTests/SupportingDataTests.cs
+++ UnitTests/SupportingDataTests.cs
@@ -1,6 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenCdsi.Schedule;
-using System;
 using System.Collections.Generic;
 
 namespace OpenCdsi.UnitTests
